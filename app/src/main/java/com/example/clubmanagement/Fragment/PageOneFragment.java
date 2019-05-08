b3cc285190abdf17918ad9bec0ae3f6546b87f60
Yoo <yooflyhigh@gmail.com>
2019-05-08 20:36:27 +0900

    ListView 버그 수정 #1

--- app/src/main/java/com/example/clubmanagement/Fragment/PageOneFragment.java
+++ app/src/main/java/com/example/clubmanagement/Fragment/PageOneFragment.java
@@ -10,15 +10,12 @@ import android.view.ViewGroup;
 import com.example.clubmanagement.Adapter.ListViewAdapter;
 import com.example.clubmanagement.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class PageOneFragment extends Fragment {
     private ListView listview ;
     private ListViewAdapter adapter;
-    private int[] img = {R.drawable.one,R.drawable.two,R.drawable.three};
-    private String[] Title = {"정준일 바램","윤종신 좋니","러블리즈 아츄"};
-    private String[] Context = {"정준일 명곡","윤종신 히트곡","러블리즈 히트곡"};
+    private int[] img = {R.drawable.one,R.drawable.two,R.drawable.three,R.drawable.three,R.drawable.three,R.drawable.three,R.drawable.three,R.drawable.three,R.drawable.three,R.drawable.three};
+    private String[] Title = {"정준일 하이","윤종신 좋니","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄"};
+    private String[] Context = {"정준일 명곡","윤종신 히트곡","러블리즈 히트곡","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄","러블리즈 아츄"};
 
     public static PageOneFragment newInstance(){
         Bundle args = new Bundle();
@@ -30,8 +27,8 @@ public class PageOneFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_page_three, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_page_one, container, false);
 
         //변수 초기화
         adapter = new ListViewAdapter();
@@ -39,12 +36,10 @@ public class PageOneFragment extends Fragment {
 
         //어뎁터 할당
         listview.setAdapter(adapter);
-
         //adapter를 통한 값 전달
         for(int i=0; i<img.length;i++){
             adapter.addVO(ContextCompat.getDrawable(this.getActivity() ,img[i]),Title[i],Context[i]);
         }
-
         return v;
     }
 
