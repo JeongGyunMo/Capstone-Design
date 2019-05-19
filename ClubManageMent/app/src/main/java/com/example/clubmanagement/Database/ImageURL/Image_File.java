d46b05a0eb83f3316ef03c7042396b8fbd3afa22
Yoo <yooflyhigh@gmail.com>
2019-05-19 22:31:26 +0900

    dbURL 추가 및 이미지 불러오기 성공, php, sql문 수정

--- ClubManageMent/app/src/main/java/com/example/clubmanagement/Database/ImageURL/Image_File.java
+++ ClubManageMent/app/src/main/java/com/example/clubmanagement/Database/ImageURL/Image_File.java
@@ -1,48 +1,48 @@
-
 package com.example.clubmanagement.Database.ImageURL;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
 import android.os.Handler;
 import android.os.Message;
-import android.widget.ImageView;
 
 public class Image_File implements Runnable{
     // 1. 변수 선언
-    ImageView img1;
-    Bitmap bitmap; // 비트맵 객체
-    // 메인 스레드와 백그라운드 스레드 간의 통신
+
+    public Bitmap bitmap; // 비트맵 객체
+
     Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
-            // 서버에서 받아온 이미지를 핸들러를 경유해 이미지뷰에 비트맵 리소스 연결
-            img1.setImageBitmap(bitmap);
         }
     };
+    private String Url;
 
-    public Image_File(){
+    public Image_File(String url){
+        Url = url;
         Thread th = new Thread(Image_File.this);
-        // 동작 수행
         th.start();
     }
 
+    // 백그라운드 스레드
     @Override
     public void run() {
-        // http://192.168.0.127/resources/images/like1.png
         URL url = null;
         try{
             // 스트링 주소를 url 형식으로 변환
-            url = new URL("http://192.168.0.3/club_icon/노네임.png");
+            url = new URL(Url);
             // url에 접속 시도
             HttpURLConnection conn = (HttpURLConnection)url.openConnection();
             conn.connect();
             // 스트림 생성
             InputStream is = conn.getInputStream();
             // 스트림에서 받은 데이터를 비트맵 변환
+
             // 인터넷에서 이미지 가져올 때는 Bitmap을 사용해야함
             bitmap = BitmapFactory.decodeStream(is);
 
@@ -51,7 +51,6 @@ public class Image_File implements Runnable{
             // 연결 종료
             is.close();
             conn.disconnect();
-
         }catch (Exception e){
             e.printStackTrace();
         }
--- ClubManageMent/app/src/main/java/com/example/clubmanagement/Fragment/PageOneFragment.java
+++ ClubManageMent/app/src/main/java/com/example/clubmanagement/Fragment/PageOneFragment.java
@@ -1,5 +1,7 @@
 package com.example.clubmanagement.Fragment;
 
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.widget.ListView;
@@ -11,10 +13,13 @@ import android.widget.Toast;
 
 import com.example.clubmanagement.Adapter.ListViewAdapter;
 import com.example.clubmanagement.Database.ClubData;
+import com.example.clubmanagement.Database.ImageURL.Image_File;
 import com.example.clubmanagement.R;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+
+import static java.lang.Thread.sleep;
 
 public class PageOneFragment extends Fragment {
     private ListView listview;
@@ -22,7 +27,7 @@ public class PageOneFragment extends Fragment {
     HashMap<String, String> Club_Item = new HashMap<String, String>();
     ArrayList<HashMap<String, String>> Club_Item_list;
     ClubData Cd = new ClubData();
-
+    Image_File ht;
     public static PageOneFragment newInstance() {
         Bundle args = new Bundle();
         PageOneFragment fragment = new PageOneFragment();
@@ -41,20 +46,38 @@ public class PageOneFragment extends Fragment {
         listview = (ListView) v.findViewById(R.id.List_view);
 
         //어뎁터 할당
-        /*
         listview.setAdapter(adapter);
+
+/*
+        url = "http://192.168.0.3/club_icon/노네임.png";
+        ht = new Image_File(url);
+        TossImage(ht, url);
+        ht.run();
+        Drawable Club_Image = new BitmapDrawable(getResources(), ht.bitmap);
+        /*
+        ImageView img1;
+        img1 = (ImageView)v.findViewById(R.id.imim);
+        img1.setImageBitmap(ht.bitmap);
+*/
+
         Cd.ClearListData();
-        Toast.makeText(getActivity(), "size = " + Club_Item.size(), Toast.LENGTH_LONG).show();
         String temp = Cd.Temp;
         Cd.GetListData(temp);
         Club_Item_list = Cd.Club_Item_list;
 
         for (int i = 0; i < Club_Item_list.size(); i++) {
             Club_Item = Club_Item_list.get(i);
-            adapter.addVO(ContextCompat.getDrawable(this.getActivity(), R.drawable.one), Club_Item.get("CLUB_ID"), Club_Item.get("CLUB_NM"));
+            String url = Club_Item.get("INTRO_FILE_NM");
+            ht = new Image_File(url);
+            ht.run();
+            try {
+                sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            adapter.addVO(new BitmapDrawable(getResources(), ht.bitmap), Club_Item.get("CLUB_ID"), Club_Item.get("CLUB_NM"));
         }
-*/
+
         return v;
     }
-
 }
