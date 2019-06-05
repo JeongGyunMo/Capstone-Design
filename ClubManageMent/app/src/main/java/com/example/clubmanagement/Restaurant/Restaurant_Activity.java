4e51793272b515d664087863a70fa786ac7811f8
junhyeok <kjh048859@gmail.com>
2019-06-05 20:15:33 +0900

    배달음식 오류 수정

--- ClubManageMent/app/src/main/java/com/example/clubmanagement/Restaurant/Restaurant_Activity.java
+++ ClubManageMent/app/src/main/java/com/example/clubmanagement/Restaurant/Restaurant_Activity.java
@@ -3,6 +3,7 @@ package com.example.clubmanagement.Restaurant;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -18,6 +19,8 @@ public class Restaurant_Activity extends AppCompatActivity {
     Button back;
     private ListRestaurantAdapter adapter_rest;
     private ListView listview;
+    private String store [] = {"도스마스","대암감자탕","룡의부활","병천순대국","이삭토스트","한솥","함께밥상"};
+    private String number [] = {"033-0000-0000", "033-0001-0001", "033-0002-0002", "033-0003-0003", "033-0004-0004", "033-0005-0005", "033-0006-0006"};
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -29,7 +32,7 @@ public class Restaurant_Activity extends AppCompatActivity {
         listview.setAdapter(adapter_rest);
         //back = (Button)findViewById(R.id.back); // 팝업 버튼 아이디
         //back.setOnClickListener();
-        adapter_rest.addVO("룡의 부활",  "033-0001-0001");
+        DataInput();
 
     }
 
@@ -43,5 +46,10 @@ public class Restaurant_Activity extends AppCompatActivity {
         if(view == back){
             startActivity(new Intent(this, Club_page.class));
         }
+    }
+
+    private void DataInput(){
+        listview.setAdapter(adapter_rest);
+        adapter_rest.addVO(store[0], number[0]);
     }
 }
